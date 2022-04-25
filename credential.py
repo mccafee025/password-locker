62fb15d2e68b1296981fced8d15aca764bd5c36c
mccafee025 <ademaamanka@gmail.com>
2022-04-25 16:04:03 +0300

    show passwords

--- credential.py
+++ credential.py
@@ -3,30 +3,38 @@ import pyperclip
 import string
 
 global users_list
+
 class User:
     users_list = []
-	def __init__(self,first_name,last_name,password):
-		self.first_name = first_name
-		self.last_name = last_name
-		self.password = password
-	def save_user(self):
-		User.users_list.append(self)
+
+    def __init__(self, first_name, last_name, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+
+    def save_user(self):
+        User.users_list.append(self)
+
 
 class Credential:
-	credentials_list =[]
-	user_credentials_list = []
-	@classmethod
-    def check_user(cls,first_name,password):
-		current_user = ''
-		for user in User.users_list:
-			if (user.first_name == first_name and user.password == password):
-				current_user = user.first_name
-		return current_user
+    credentials_list = []
+    user_credentials_list = []
 
+    @classmethod
+
+    def check_user(cls,first_name,password):
+        current_user = ''
+        for user in User.users_list:
+            if (user.first_name == first_name and user.password == password):
+                current_user = user.first_name
+        return current_user
     def __init__(self,user_name,site_name,account_name,password):
-		self.user_name = user_name
-		self.site_name = site_name
-		self.account_name = account_name
-		self.password = password
+        self.user_name = user_name
+        self.site_name = site_name
+        self.account_name = account_name
+        self.password = password
     def save_credentials(self):
-		Credential.credentials_list.append(self)
+        Credential.credentials_list.append(self)
+    def generate_password(size=8, char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
+        gen_pass=''.join(random.choice(char) for _ in range(size))
+        return gen_pass
