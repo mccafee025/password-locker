fb713ae96160f6270ab8d54cbe8de01650daa2b0
mccafee025 <ademaamanka@gmail.com>
2022-04-25 12:26:24 +0300

    add the new info after input

--- pass.py
+++ pass.py
@@ -13,4 +13,7 @@ def verify_user(first_name,password):
 def generate_password():
     gen_pass = Credential.generate_password()
     return gen_pass
+def create_credential(user_name,site_name,account_name,password):
+    new_credential=Credential(user_name,site_name,account_name,password)
+    return new_credential
 
