3eb23ebaf251a322ac27e945bd547e59243332ee
mccafee025 <ademaamanka@gmail.com>
2022-04-25 15:17:21 +0300

    add function to save new user info

--- credential.py
+++ credential.py
@@ -4,3 +4,10 @@ import string
 
 global users_list
 class User:
+    users_list = []
+	def __init__(self,first_name,last_name,password):
+		self.first_name = first_name
+		self.last_name = last_name
+		self.password = password
+	def save_user(self):
+		User.users_list.append(self)
