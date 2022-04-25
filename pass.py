f213253b27cb26a9d44277a017956ff31d628391
mccafee025 <ademaamanka@gmail.com>
2022-04-25 10:34:10 +0300

    add function to save user

--- pass.py
+++ pass.py
@@ -5,4 +5,6 @@ from credentials import User, Credential
 def create_user(fname,lname,password):
     new_user = User(fname,lname,password)
     return new_user
+def save_user(user):
+    User.save_user(user)
 
