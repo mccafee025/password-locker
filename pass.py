b993eade5021fc48e4cd270f7faf1fc5551b43fb
mccafee025 <ademaamanka@gmail.com>
2022-04-25 10:29:51 +0300

    add function to create new user

--- pass.py
+++ pass.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python
 import pyperclip
 from credentials import User, Credential 
+
+def create_user(fname,lname,password):
+    new_user = User(fname,lname,password)
+    return new_user
+
